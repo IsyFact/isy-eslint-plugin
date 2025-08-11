a7f5209bcefc66cffcc7d6a944bacdc4e7299fe8
mtpham-collab <74294456+mtpham-collab@users.noreply.github.com>
2025-08-11 13:34:36 +0200

    feat: IFS-4827 peer dependencies updated and switched to flat config
    * feat: IFS-4827 peer dependencies updated and switched to flat config
    
    * feat: IFS-4827 peer dependencies updated
    
    * feat: IFS-4827 readme and config updated
    
    * feat: IFS-4827 readme and config updated
    
    * feat: IFS-4827 readme and config updated
    
    * feat: IFS-4827: configs updated
    
    * feat: IFS-4827: readme updated

---
+++ configs/base.config.js
@@ -0,0 +1,25 @@
+const tseslint = require('typescript-eslint');
+
+module.exports = [
+  { ignores: ['**/*.tsx'] },
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+
+  {
+    files: ['**/*.ts'],
+    ...tseslint.configs.eslintRecommended,
+  },
+
+  ...tseslint.configs.recommendedTypeChecked.map(cfg => ({
+    ...cfg,
+    files: [...(cfg.files || []), '**/*.ts'],
+  })),
+];
